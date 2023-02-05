67c82c2f87288da2aac668b4a9a5c3d5f2967380
Nastya Piunova <piunovaam@yandex.ru>
2023-02-05 19:07:15 +0300

    Seminar 2

---
+++ Homeworks/Seminar_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: 
+// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа
+
+Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 & number < 1000)
+{
+    int digit_2 = (number / 10) % 10;
+    Console.WriteLine($"Second digit is {digit_2}");
+}
+else
+{
+    Console.WriteLine("Your number isn't 3-digit!");
+}
+

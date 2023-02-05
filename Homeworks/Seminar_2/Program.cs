57e389806ed769a95db0bdd87e9ed6c608ea4067
Nastya Piunova <piunovaam@yandex.ru>
2023-02-05 20:25:51 +0300

    Update Seminar 2

--- Homeworks/Seminar_2/Program.cs
+++ Homeworks/Seminar_2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 10: 
 // Напишите программу, которая принимает на вход трёхзначное число
 // и на выходе показывает вторую цифру этого числа
-
+/*
 Console.Write("Input your number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -10,8 +10,26 @@ if (number > 99 & number < 1000)
     int digit_2 = (number / 10) % 10;
     Console.WriteLine($"Second digit is {digit_2}");
 }
+*/
+
+// Задача 13:
+// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+/*
+Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 & number < 1000)
+{
+    int digit_3 = number % 10;
+    Console.WriteLine($"Third digit is {digit_3}");
+}
 else
 {
     Console.WriteLine("Your number isn't 3-digit!");
 }
-
+*/
+// Задача 15:
+// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.

a326ff1165b538ee369a8080aa39a0709f481da0
Nastya Piunova <piunovaam@yandex.ru>
2023-02-09 14:00:59 +0300

    task 21

--- Homeworks/Seminar_3/Program.cs
+++ Homeworks/Seminar_3/Program.cs
@@ -2,11 +2,12 @@
 //Напишите программу, которая принимает на вход координаты двух точек
 //и находит расстояние между ними в 3D пространстве.
 
-double DistanceAB(int xa, int ya, int xb, int yb, int xc, int yc)
+double DistanceAB(int xa, int ya, int za, int xb, int yb, int zb)
 {
-    int deltaX = xa - xb - xc;
-    int deltaY = ya - yb - xc;
-    double distAB = Math.Round(Math.Sqrt(deltaX * deltaX + deltaY * deltaY), 2);   
+    int deltaX = xa - xb;
+    int deltaY = ya - yb;
+    int deltaZ = za - zb;
+    double distAB = Math.Round(Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ), 2);   
     return distAB;
 }
 Console.Write("Input X coordinate of A: ");
@@ -15,18 +16,18 @@ int xACoord = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input X coordinate of B: ");
 int xBCoord = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input X coordinate of C: ");
-int xCCoord = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Input Y coordinate of A: ");
 int yACoord = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Input Y coordinate of B: ");
 int yBCoord = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input Y coordinate of C: ");
-int yCCoord = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Z coordinate of A: ");
+int zACoord = Convert.ToInt32(Console.ReadLine());
 
-double distanceAB = DistanceAB(xACoord, yACoord, xBCoord, yBCoord, xCCoord, yCCoord);
+Console.Write("Input Z coordinate of B: ");
+int zBCoord = Convert.ToInt32(Console.ReadLine());
+
+double distanceAB = DistanceAB(xACoord, yACoord, zACoord, xBCoord, yBCoord, zBCoord);
 
 Console.WriteLine($"Distance between A and B is {distanceAB}");
